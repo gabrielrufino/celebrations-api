23b2a1f20b1152abfd61b7c306e33491cf08ad3d
Gabriel Rufino <contato@gabrielrufino.com>
2022-06-09 08:34:38 -0300

    Removing env vars from pm2 file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,12 +5,7 @@ module.exports = {
       script: 'npm',
       args: 'start',
       env: {
-        HOST: process.env.PROD_HOST,
-        PORT: process.env.PROD_PORT,
-        APP_KEYS: process.env.PROD_APP_KEYS,
-        API_TOKEN_SALT: process.env.PROD_API_TOKEN_SALT,
-        ADMIN_JWT_SECRET: process.env.PROD_ADMIN_JWT_SECRET,
-        JWT_SECRET: process.env.PROD_JWT_SECRET
+        NODE_ENV: 'production'
       }
     },
   ]

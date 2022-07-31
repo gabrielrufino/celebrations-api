881170c1c56d861c29b12147738372a7e4ea8cf1
Gabriel Rufino <contato@gabrielrufino.com>
2022-07-31 18:29:57 -0300

    Using only postgres

--- config/database.js
+++ config/database.js
@@ -1,25 +1,11 @@
 const path = require('path');
 
-module.exports = ({ env }) => {
-  const client = env('DATABASE_CLIENT', 'sqlite');
-  const connections = {
-    sqlite: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db'))
-    },
-    postgres: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'strapi'),
-      user: env('DATABASE_USERNAME', 'strapi'),
-      password: env('DATABASE_PASSWORD', 'strapi'),
-    },
-  }
-
-  return ({
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'postgres',
     connection: {
-      client,
-      connection: connections[client],
-      useNullAsDefault: true,
+      url: env('DATABASE_URL', 'postgres://root:root@localhost:5432/celebrations_api')
     },
-  })
-};
+    useNullAsDefault: true,
+  },
+});

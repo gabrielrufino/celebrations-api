b36c8a8267e3808ff2b2d4e2968921bd6dd2c48f
Gabriel Rufino <contato@gabrielrufino.com>
2022-07-31 22:02:55 -0300

    Update database config

--- config/database.js
+++ config/database.js
@@ -1,9 +1,7 @@
 module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
-    connection: {
-      url: env('DATABASE_URL', 'postgres://root:root@localhost:5432/celebrations_api')
-    },
+    connection: env('DATABASE_URL', 'postgres://root:root@localhost:5432/celebrations_api'),
     useNullAsDefault: true,
   },
 });

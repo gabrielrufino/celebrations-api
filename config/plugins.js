c707425b85dc36732f6b1fe37c9a8ab38e0f3894
Gabriel Rufino <contato@gabrielrufino.com>
2022-08-02 19:54:44 -0300

    Email

--- config/plugins.js
+++ config/plugins.js
@@ -1,5 +1,22 @@
-module.exports = () => ({
+module.exports = ({ env }) => ({
   'duplicate-button': true,
+  email: {
+    config: {
+      provider: 'nodemailer',
+      providerOptions: {
+        host: env('SMTP_HOST', 'smtp.example.com'),
+        port: env.int('SMTP_PORT', 587),
+        auth: {
+          user: env('SMTP_USERNAME', 'email@example.com'),
+          pass: env('SMTP_PASSWORD', '12345678'),
+        },
+      },
+      settings: {
+        defaultFrom: 'no-reply@celebrationsapi.dev',
+        defaultReplyTo: 'contact@celebrationsapi.dev',
+      },
+    },
+  },
   'entity-notes': {
 		enabled: true,
 	},
